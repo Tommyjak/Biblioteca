33a694509139e0bc2eaa107379ab29496ad3e578
3f17 <3f17@Liceospalla.local>
2017-03-06 11:53:57 +0100

    proprieta, classi

--- Biblioteca/Biblioteca/Libro.cs
+++ Biblioteca/Biblioteca/Libro.cs
@@ -8,5 +8,16 @@ namespace Biblioteca
 {
     class Libro
     {
+        private string _titolo;
+        public string titolo { get; set; }
+
+        private string _autore;
+        public string autore { get; set; }
+
+        private string _genere;
+        public string genere { get; set; }
+
+        private string _isbn;
+        public string isbn { get; set; }
     }
 }
--- Biblioteca/Biblioteca/Utente.cs
+++ Biblioteca/Biblioteca/Utente.cs
@@ -8,6 +8,18 @@ namespace Biblioteca
 {
     class Utente
     {
-        bhterhnrynj
+        private string _nome;
+        public string nome { get; set; }
+
+        private string _cognome;
+        public string cognome { get; set; }
+
+        private string _codFisc;
+        public string codFisc { get; set; }
+
+        private DateTime _dataNasc;
+        public DateTime dataNasc { get; set; }
+
+        public string describe()
     }
 }
