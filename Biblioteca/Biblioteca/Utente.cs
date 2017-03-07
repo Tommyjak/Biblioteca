8db230b30ac0efe53b08b4a8fd624b62daefeda1
Silvia Trevisan <Silvia Trevisan>
2017-03-07 16:59:34 +0100

    Implementazione classe Utente

--- Biblioteca/Biblioteca/Utente.cs
+++ Biblioteca/Biblioteca/Utente.cs
@@ -1,25 +1,54 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
 
 namespace Biblioteca
 {
     class Utente
     {
         private string _nome;
-        public string nome { get; set; }
+        public string nome { get { return _nome; } }
 
         private string _cognome;
-        public string cognome { get; set; }
+        public string cognome { get { return _cognome; } }
 
         private string _codFisc;
-        public string codFisc { get; set; }
+        public string codFisc { get { return _codFisc; } }
 
-        private DateTime _dataNasc;
-        public DateTime dataNasc { get; set; }
+        private DateTime _dataNascita;
+        public DateTime dataNascita { get; set; }
+
+        //public List<Libro> = libriUtente;
+
+        public Utente (string nome, string cognome, string codFisc, DateTime dataNascita)
+        {
+            _nome = nome;
+            _cognome = cognome;
+            _codFisc = codFisc;
+            _dataNascita = dataNascita;
+
+        }
+
+        public override string ToString()
+        {
+            return (nome + ", " + cognome + ", " + dataNascita + ".");
+        }
 
         public string describe()
+        {
+            string output = "Nome: " + nome + ";\r\n";
+                   output += "Cognome: "+ cognome + ";\r\n";
+                   output += "Data di Nascita: " + dataNascita + ";\r\n";
+                   output += "Codice Fiscale: " + codFisc + ";\r\n";
+                   output += "Numero di libri posseduti: " + "(Index lista libriUtente)" + ";\r\n";
+
+            return output;
+        }
+
+        public string describeLibriUtente()
+        {
+            string output = "Descrizione lista libriUtente";
+            return output;
+        }
     }
 }
